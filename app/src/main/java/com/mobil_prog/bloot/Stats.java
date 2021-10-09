bc95607be489d582254e5d16ec908f775d28c2f9
Attila772 <71554727+Attila772@users.noreply.github.com>
2021-10-09 17:09:52 +0200

    graph

--- app/src/main/java/com/mobil_prog/bloot/Stats.java
+++ app/src/main/java/com/mobil_prog/bloot/Stats.java
@@ -3,15 +3,38 @@ package com.mobil_prog.bloot;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class Stats extends AppCompatActivity {
+
+    LineGraphSeries<DataPoint> series;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        double y,x;
+        x= -5.0;
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        for(int i =0;i<500;i++){
+            x=x+0.1;
+            y= Math.sin(x);
+            series.appendData(new DataPoint(x,y),true,500);
+        }
+
+        graph.getGridLabelRenderer().setGridColor(Color.BLACK);
+        graph.getGridLabelRenderer().setHighlightZeroLines(false);
+        graph.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLACK);
+        graph.getGridLabelRenderer().setVerticalLabelsColor(Color.BLACK);
+        graph.getGridLabelRenderer().reloadStyles();
+        graph.addSeries(series);
     }
     public void Back_from_stats (View v){
         Intent intent = new Intent(this, Home_activity.class);
