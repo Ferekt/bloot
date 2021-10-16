f1d9d4716b385a704590584ab09e75217282eca1
Attila772 <71554727+Attila772@users.noreply.github.com>
2021-10-16 17:54:40 +0200

    transactions

--- app/src/main/java/com/mobil_prog/bloot/Stats.java
+++ app/src/main/java/com/mobil_prog/bloot/Stats.java
@@ -5,8 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import com.jjoe64.graphview.GraphView;
 import org.eazegraph.lib.charts.BarChart;
 import org.eazegraph.lib.charts.PieChart;
@@ -16,14 +34,85 @@ import org.eazegraph.lib.models.PieModel;
 import org.eazegraph.lib.models.StackedBarModel;
 
 public class Stats extends AppCompatActivity {
+    public ArrayList<Transaction> tran_list = new ArrayList<Transaction>();
+    public void postRequest(String postUrl,RequestBody postbody){
+        OkHttpClient client = new OkHttpClient();
 
+        final Request request = new Request.Builder()
+                .url(postUrl)
+                .post(postbody)
+                .header("Accept","application/json")
+                .header("Content-type", "application/json")
+                .build();
 
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+                Log.d("Fail",e.getMessage());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(Stats.this, "Cannot reach server", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            JSONArray myResponse = new JSONArray();
+            ArrayList<JSONObject> jObj = new ArrayList<JSONObject>();
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    myResponse = new JSONArray(response.body().string());
+                    JSONObject asd = myResponse.getJSONObject(0);
+                    Log.e("AAAAA",asd.getString("date").toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    for(int i=0;i<myResponse.length();i++){
+                        JSONObject asd = (JSONObject)myResponse.get(i);
+                        jObj.add(asd);
+                    }
+
+                    for (JSONObject temp: jObj)
+                    {
+                        String sender = temp.getString("sender");
+                        String reciever = temp.getString("reciever");
+                        int value = temp.getInt("value");
+                        SimpleDateFormat formatter = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+                        Date date = formatter.parse(temp.getString("date"));
+                        Transaction tran = new Transaction(sender,value,reciever,date);
+                        tran_list.add(tran);
+                        Log.e("DATE" ,date.toString());
+                    }
+
+                } catch (JSONException | ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
         BarChart mBarChart = (BarChart) findViewById(R.id.barchart);
+
+        JSONObject reg_form = new JSONObject();
+        try {
+            reg_form.put("subject", "group_tran");
+            reg_form.put("group", MainActivity.group);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(MediaType.parse("application/json; charset= utf-8"), reg_form.toString());
+        postRequest(MainActivity.postUrl, body);
+
+
+
+
 
         mBarChart.addBar(new BarModel(2.3f, 0xFF123456));
         mBarChart.addBar(new BarModel(2.f,  0xFF343456));
