70bea97d9203b9b3fd77afc53bd87d1e3e6772c7
Ferekt <78617056+Ferekt@users.noreply.github.com>
2021-10-16 19:30:28 +0200

    g

--- app/src/main/java/com/mobil_prog/bloot/Stats.java
+++ app/src/main/java/com/mobil_prog/bloot/Stats.java
@@ -9,6 +9,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -17,6 +20,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -104,7 +108,6 @@ public class Stats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
-        BarChart mBarChart = (BarChart) findViewById(R.id.barchart);
 
         JSONObject reg_form = new JSONObject();
         try {
@@ -116,65 +119,23 @@ public class Stats extends AppCompatActivity {
         }
         RequestBody body = RequestBody.create(MediaType.parse("application/json; charset= utf-8"), reg_form.toString());
         postRequest(MainActivity.postUrl, body);
-
-
-
-
-
-        mBarChart.addBar(new BarModel(2.3f, 0xFF123456));
-        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
-        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
-        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
-        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
-        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
-        mBarChart.addBar(new BarModel(0.4f, 0xFF1FF4AC));
-        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
-
-        mBarChart.startAnimation();
-
-        StackedBarChart mStackedBarChart = (StackedBarChart) findViewById(R.id.stackedbarchart);
-
-        StackedBarModel s1 = new StackedBarModel("12.4");
-
-        s1.addBar(new BarModel(2.3f, 0xFF63CBB0));
-        s1.addBar(new BarModel(2.3f, 0xFF56B7F1));
-        s1.addBar(new BarModel(2.3f, 0xFFCDA67F));
-
-        StackedBarModel s2 = new StackedBarModel("13.4");
-        s2.addBar(new BarModel(1.1f, 0xFF63CBB0));
-        s2.addBar(new BarModel(2.7f, 0xFF56B7F1));
-        s2.addBar(new BarModel(0.7f, 0xFFCDA67F));
-
-        StackedBarModel s3 = new StackedBarModel("14.4");
-
-        s3.addBar(new BarModel(2.3f, 0xFF63CBB0));
-        s3.addBar(new BarModel(2.f, 0xFF56B7F1));
-        s3.addBar(new BarModel(3.3f, 0xFFCDA67F));
-
-        StackedBarModel s4 = new StackedBarModel("15.4");
-        s4.addBar(new BarModel(1.f, 0xFF63CBB0));
-        s4.addBar(new BarModel(4.2f, 0xFF56B7F1));
-        s4.addBar(new BarModel(2.1f, 0xFFCDA67F));
-
-        mStackedBarChart.addBar(s1);
-        mStackedBarChart.addBar(s2);
-        mStackedBarChart.addBar(s3);
-        mStackedBarChart.addBar(s4);
-
-        mStackedBarChart.startAnimation();
-
-        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
-
-        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
-        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
-        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
-        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
-
-        mPieChart.startAnimation();
     }
     public void Back_from_stats (View v){
         Intent intent = new Intent(this, Home_activity.class);
         startActivity(intent);
         this.finish();
     }
+    public void setBarCharts(ArrayList<Transaction>tran, ValueLineChart mLineChart){
+        List <String> names=new ArrayList<String>();
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
+        int value=0;
+        for (Transaction t:tran) {
+            if (t.sender.equals(MainActivity.user)){
+                value+=t.value;
+                series.addPoint(new ValueLinePoint(String.valueOf(t.date.getHours())+String.valueOf(t.date.getMinutes()),value));
+            }
+        }
+    }
 }
+
